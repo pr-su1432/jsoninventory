6299ab0bde6fbeaa2220aeb475fda77cd9bdc26f
[prasanna] <prasanna>
2022-05-25 17:35:16 +0530

    [prasanna]:calculating items in json

---
+++ jsoninventory/inventorymodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsoninventory
+{
+    internal class inventorymodel
+    {
+        public string name { get; set; }
+        public int Weight { get; set; }
+        public int price { get; set; }
+    }
+}
---
+++ jsoninventory/jsoninventorymain.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsoninventory
+{
+    internal class jsoninventorymain
+    {
+        inventorymodel model = new inventorymodel();
+        List<inventorymodel> list = new List<inventorymodel>();
+       
+
+        public void convert(string jfilepath)
+        {
+
+            using (StreamReader file = new StreamReader(jfilepath))
+            {
+                var json = file.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<inventorymodel>>(json);
+                foreach (var objects in items)
+                {
+                    Console.WriteLine(objects.name + " " + objects.price + " " + objects.Weight);
+                   
+                }
+            }
+        }
+    }
+}
